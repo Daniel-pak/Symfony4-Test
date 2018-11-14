a6178adbbf7dde85e7ab5d840be7662ce438d913
Daniel Pak <PakJuhnMan@gmail.com>
2018-11-14 14:25:17 -0700

    added in .env to .gitignore and added in mailer class with Swift Mailer functions

--- analytics-dash/src/Controller/ShowDataController.php
+++ analytics-dash/src/Controller/ShowDataController.php
@@ -4,16 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class ShowDataController extends AbstractController
 {
     /**
      * @Route("/show/data", name="show_data")
      */
-    public function index()
+    public function index(SessionInterface $session)
     {
         return $this->render('show_data/index.html.twig', [
             'controller_name' => 'ShowDataController',
+            'name' => $session->get('result')
         ]);
     }
 }
